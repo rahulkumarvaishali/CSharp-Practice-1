a4835a02084568eb1b1e1e4776c0a79318c32584
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-18 02:13:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static bool test(int num)
+    {
+
+        if (num == 0)
+
+            return false;
+
+        if (num == 0)
+
+            return false;
+        while (num != 1)
+        {
+
+            if (num !=0)
+
+                return false;
+
+            num = num / 2;
+        }
+        return true;
+    }
+    public static void Main()
+    {
+        Console.Write("Enter the number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        test(num);
+        Console.ReadLine();
+    }
+}
