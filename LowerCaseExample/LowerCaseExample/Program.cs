5d8def651237f976d562a430706cc00d254eddb0
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-18 01:35:23 +0530

    LowerCaseExample

---
+++ LowerCaseExample/LowerCaseExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LowerCaseExample
+{
+    internal class Program
+    {
+        static void CheckLower()
+        {
+            string str;
+            char ch;
+            System.Console.Write("Enter the string: ");
+            str = System.Console.ReadLine();
+            int l = str.Length;
+            for (int i = 0; i < l; i++)
+            {
+                ch = str[i];
+                if (char.IsUpper(ch))
+                {
+                    System.Console.Write(char.ToLower(ch));
+                }
+                else
+                    System.Console.Write(ch);
+            }
+        }
+        static void Main(string[] args)
+        {
+            CheckLower();
+            System.Console.ReadLine();
+        }
+    }
+}
+//  Program Output 
+//  Enter the string: AbHIsHEk
+// abhishek
+
