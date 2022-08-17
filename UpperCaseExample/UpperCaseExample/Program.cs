1ba1114d7f0b3518a0891456ebccd8836d212814
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-18 01:28:06 +0530

    UpperCaseExample
    * Program Output 
    * Enter the string: Abhishek
    * ABHISHEK

---
+++ UpperCaseExample/UpperCaseExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UpperCaseExample
+{
+    internal class Program
+    {
+        static void CheckUpper()
+            {
+                string str;
+                char ch;
+                System.Console.Write("Enter the string: ");
+                str = System.Console.ReadLine();
+                int l = str.Length; 
+                for(int i = 0; i < l; i++)
+                {
+                    ch = str[i];
+                    if(char.IsLower(ch))
+                    {
+                        System.Console.Write(char.ToUpper(ch));
+                    }
+                    else
+                        System.Console.Write(ch);
+                }
+            }
+            static void Main(string[] args)
+            {
+                CheckUpper();
+                System.Console.ReadLine();
+            }
+        }
+    }
