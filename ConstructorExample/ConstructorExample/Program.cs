9ca2c55d0b61b1b0635bc5ff9143827ac0ea62bd
Abhishek Thakur <75131439+Abhishek95725@users.noreply.github.com>
2022-08-18 01:37:41 +0530

    ConstructorExample

---
+++ ConstructorExample/ConstructorExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorExample
+{
+    internal class Program
+    {
+        int id;
+        String name;
+        float salary;
+        public Program(int i, String n, float s)
+        {
+            id = i;
+            name = n;
+            salary = s;
+            Console.WriteLine(i);
+            Console.WriteLine(n);
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program(10, "Thakur Abhishek Singh", 957.25f);
+            Console.ReadLine();
+        }
+    }
+}
